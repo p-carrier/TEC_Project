d6ef97268d3fff73e1896b8c9e39f1cc6d05087e
p-carrier <p-carrier@users.noreply.github.com>
2023-05-29 11:38:46 -0400

    implement complete workflow - fetching, fromatting and saving

--- src/main/java/DataExtractor.java
+++ src/main/java/DataExtractor.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class DataExtractor {
+import dto.Location;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class DataExtractor {
+    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException, SQLException {
+        ArrayList<Location> locations = DataController.fetchData();
+        DataRepository repository = new DataRepository();
+        repository.saveAllLocations(locations);
+    }
 }
